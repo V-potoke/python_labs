a2ed6a2617e1e609e63316276338c68c911eeec2
V-potoke <ruslestvig@gmail.com>
2025-10-08 15:03:27 +0300

    :commit02

---
+++ src/lab02/arrays.py
@@ -0,0 +1,40 @@
+def min_max(a):
+    if not a:
+        return ValueError
+    return (min(a), max(a))
+
+
+def unique_sorted(a):
+    return sorted(set(a))
+
+
+def flatten(a):
+    row_major = []
+    fl = False
+    for a1 in a:
+        if not isinstance(a1, (list, tuple)):
+            fl = True
+            break
+        for i in range(len(a1)):
+            row_major.append(a1[i])
+    if fl:
+        return TypeError
+    return row_major
+
+
+print('min_max')
+print(min_max([3, -1, 5, 5, 0]))
+print(min_max([42]))
+print(min_max([-5, -2, -9]))
+print(min_max([]))
+print(min_max([1.5, 2, 2.0, -3.1]))
+print('unique_sorted')
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+print('flatten')
+print(flatten([[1, 2], [3, 4]]))
+print(flatten([[1, 2], (3, 4, 5)]))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
---
+++ src/lab02/matrix.py
@@ -0,0 +1,50 @@
+def transpose(a):
+    if a == []:
+        return []
+    if len(set(len(a1) for a1 in a)) != 1:
+        return ValueError
+    a_res = []
+    for i in range(len(a[0])):
+        new_list = []
+        for k in range(len(a)):
+            new_list.append(a[k][i])
+        a_res.append(new_list)
+    return a_res
+
+
+def row_sums(a):
+    if len(set(len(a1) for a1 in a)) != 1:
+        return ValueError
+    return [sum(a1) for a1 in a]
+
+
+def col_sums(a):
+    if a == []:
+        return []
+    if len(set(len(a1) for a1 in a)) != 1:
+        return ValueError
+    a_res = []
+    for i in range(len(a[0])):
+        new_list = []
+        for k in range(len(a)):
+            new_list.append(a[k][i])
+        a_res.append(new_list)
+    return [sum(a1) for a1 in a_res]
+
+
+print('transpose')
+print(transpose([[1, 2, 3]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
+print('row_sums')
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [10, -10]]))
+print(row_sums([[0, 0], [0, 0]]))
+print(row_sums([[1, 2], [3]]))
+print('col_sums')
+print(col_sums([[1, 2, 3], [4, 5, 6]]))
+print(col_sums([[-1, 1], [10, -10]]))
+print(col_sums([[0, 0], [0, 0]]))
+print(col_sums([[1, 2], [3]]))
